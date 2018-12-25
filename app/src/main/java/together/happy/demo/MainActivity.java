0f2176aabfc300cee1ec67d0a7200ec528f25496
ZengHui <czh.git@outlook.com>
2018-12-25 16:33:26 +0800

    Initial commit

---
+++ app/src/main/java/together/happy/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package together.happy.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
