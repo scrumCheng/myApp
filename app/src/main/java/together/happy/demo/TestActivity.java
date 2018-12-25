44fd9a8ff01eaefe9a9a95c39bea5520ad9d0eb0
ZengHui <czh.git@outlook.com>
2018-12-25 16:51:31 +0800

    add test activity

---
+++ app/src/main/java/together/happy/demo/TestActivity.java
@@ -0,0 +1,13 @@
+package together.happy.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
